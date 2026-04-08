d097201ba40cdbc4c2351db993ae93b09262b8b6
Cosmophasis <colinchiu62@gmail.com>
2026-04-08 15:55:04 -0600

    Fix mistake in name parsing

--- scripts/Concatenated_VCF_tree.sh
+++ scripts/Concatenated_VCF_tree.sh
@@ -17,7 +17,7 @@ VCF=$(awk -v num="$SLURM_ARRAY_TASK_ID" 'NR==num' ~/tmp/bcf.list)
 #Chr="CM046102.1,CM046103.1,CM046104.1,CM046105.1,CM046106.1,CM046107.1,CM046108.1,CM046109.1,CM046110.1,CM046111.1,CM046112.1,CM046113.1,CM046114.1,CM046115.1,CM046116.1,CM046117.1,CM046118.1,CM046119.1,CM046120.1,CM046121.1,CM046122.1,CM046123.1,CM046124.1,CM046125.1,CM046126.1,CM046127.1,CM046128.1,CM046129.1,CM046130.1"
 outDir=~/scratch/Phylogenomics/Concatenated/
 
-name=$(basename "$VCF" | awk '{ print $1"_"$2 }')
+name=$(basename "$VCF" | awk -F"_" '{ print $1"_"$2 }')
 
 # Output a temporary file to the node local directory
 bcftools view --threads "$SLURM_CPUS_PER_TASK" \
